794477451b53b0f1268241d0bf6c5d697e3541d9
Mohammad <mohammadbabaeesem@gmail.com>
2022-07-08 15:27:49 +0430

    Create front for afshan

---
+++ todo_sarab/lib/main.dart
@@ -0,0 +1,146 @@
+// ignore_for_file: deprecated_member_use
+
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class Album {
+  final String title;
+  final String description;
+  final DateTime creation_date;
+  final DateTime todo_date;
+  final bool compeleted;
+
+  const Album({
+    required this.title,
+    required this.description,
+    required this.creation_date,
+    required this.todo_date,
+    required this.compeleted,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      title: json['title'],
+      description: json['description'],
+      creation_date: json['creation_date'],
+      todo_date: json['todo_date'],
+      compeleted: json['compeleted'],
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Sarab Todo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // The list that contains information about photos
+  // ignore: unused_field
+  List _loadedPhotos = [];
+
+  // The function that fetches data from the API
+  Future<void> _fetchData() async {
+    // ignore: constant_identifier_names
+    const API_URL = 'http://127.0.0.1:8000/api/v1/task/task-list/';
+
+    HttpClient client = new HttpClient();
+    client.autoUncompress = true;
+
+    final HttpClientRequest request = await client.getUrl(Uri.parse(API_URL));
+    request.headers
+        .set(HttpHeaders.contentTypeHeader, "application/json; charset=UTF-8");
+    final HttpClientResponse response = await request.close();
+
+    final String content = await response.transform(utf8.decoder).join();
+    final List data = json.decode(content);
+
+    setState(() {
+      _loadedPhotos = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void _showDialog() {
+      // flutter defined function
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: const Text("Alert Dialog title"),
+            content: const Text("Alert Dialog body"),
+            actions: <Widget>[
+              // usually buttons at the bottom of the dialog
+              FlatButton(
+                child: const Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SizedBox(
+          child: ListView.builder(
+        itemCount: _loadedPhotos.length,
+        itemBuilder: (BuildContext ctx, index) {
+          return ListTile(
+            leading: Image.network(
+              _loadedPhotos[index]["thumbnailUrl"],
+              width: 150,
+              fit: BoxFit.cover,
+            ),
+            title: Text(_loadedPhotos[index]['title']),
+            subtitle:
+                Text('Description: ${_loadedPhotos[index]["description"]}'),
+          );
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _fetchData();
+          _showDialog();
+        },
+        tooltip: 'Create Task',
+        child: const Icon(Icons.add),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
